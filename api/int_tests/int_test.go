c11844d676ede6199e1bc4899d27e234c13ae68b
Ahad Shabbir <ashabbir@mailbox.org>
2023-03-28 20:32:27 -0500

    setup docker test

--- api/int_tests/int_test.go
+++ api/int_tests/int_test.go
@@ -12,8 +12,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var client *mongo.Client
-
 func TestMain(m *testing.M) {
 	pool, err := dockertest.NewPool("")
 	if err != nil {
@@ -25,30 +23,31 @@ func TestMain(m *testing.M) {
 		log.Fatalf("Could not connect to Docker: %s", err)
 	}
 
-	resource, err := pool.Run("mongo", "latest")
+	resource, err := pool.Run("mongo", "latest", nil)
 	if err != nil {
 		log.Fatalf("Could not start resource: %s", err)
 	}
 
-	client, err := mongo.NewClient(
-		options.Client().ApplyURI(
-			fmt.Sprintf("mongodb://localhost:%s/", resource.GetPort("27017/tcp"))))
-	if err != nil {
-		return err
-	}
+	defer func() {
+		if err := pool.Purge(resource); err != nil {
+			log.Fatalf("Could not purge resource: %s", err)
+		}
+	}()
 
 	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
-	if err := pool.Retry(func() error {
+	err = pool.Retry(func() error {
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 		defer cancel()
 
-		err = client.Connect(ctx)
+		client, err := mongo.Connect(ctx,
+			options.Client().ApplyURI(fmt.Sprintf("mongodb://localhost:%s", resource.GetPort("27017/tcp"))))
 		if err != nil {
 			return err
 		}
 
 		return client.Ping(ctx, nil)
-	}); err != nil {
+	})
+	if err != nil {
 		log.Fatalf("Could not connect to database: %s", err)
 	}
 }
